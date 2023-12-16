bbbaad5b94a894ff9b09be34fc57a0d37515550b
federico blautzik <fedeblau@gmail.com>
2023-12-16 16:45:16 -0300

    1233223

--- src/components/component/UploadGallery.jsx
+++ src/components/component/UploadGallery.jsx
@@ -161,7 +161,7 @@ const EventForm = () => {
                 onClick={handleCreateProduct}
                 className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
             >
-                Crear producto
+                Crear Invitación
             </button>
 
 
